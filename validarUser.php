b04568b871496c34d538a954f7fe5bad7ddbf90f
Guille <57747085+Byguillemc21@users.noreply.github.com>
2022-03-02 19:22:27 +0100

    suuu
    suuu

---
+++ validarUser.php
@@ -0,0 +1,21 @@
+<?php
+$usuarioValue=$_POST["usuari"];
+$contraValue=$_POST["contrasenya"];
+
+$dbLink=mysqli_connect("localhost","usuari","123456","goActivity") or exit(mysqli_connect_error());
+
+
+$sql="SELECT * FROM tb_usuarios WHERE Usuario='$usuarioValue' AND Contrasenya='$contraValue';";
+$result=mysqli_query($dbLink,$sql) or exit (mysqli_error($dbLink));
+
+if(mysqli_num_rows($result)==1){
+	session_start();
+	$_SESSION["usuarioSESSION"]=$usuarioValue;
+	$_SESSION["login"]=true;
+	header("Location:index.html");
+}else { ?>
+	<h3><i>El usuario o la contraseña es incorrecta</i></h3>
+	<a href=login.php>Volver a logear</a> <?php 
+}
+
+?>
