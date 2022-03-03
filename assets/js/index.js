1526901979d5f920c063d9b939a1064600ca1596
paumotos <91218054+paumotos@users.noreply.github.com>
2022-03-03 19:16:34 +0100

    sd

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,21 @@
 var contador = 0;
 
-var imagenes = ["../img/", "../img/", "../img/", "../img/", "../img/"];
+var imagenes = [
+  "../img/barco1.jpg",
+  "../img/barco2.jpeg",
+  "../img/barco3.png",
+  "../img/barco4.png",
+  "../img/barco5.png",
+];
 
 $(function () {
   setInterval(changeImage, 2000);
 });
 
 function changeImage() {
-  $("main").css("background-image", "url(" + imagenes[index] + ")");
-
+  $("main").css("background-image= url('" + imagenes[contador] + "')");
+  console.log("BARCO " + (contador + 1));
   contador++;
 
-  if (contador == 4) contador = 0;
+  if (contador == 5) contador = 0;
 }
