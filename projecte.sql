ad075d8c089894c8a360136928c3ac830e301dab
paumotos <91218054+paumotos@users.noreply.github.com>
2022-05-17 19:47:35 +0200

    dsaf

--- projecte.sql
+++ projecte.sql
@@ -19,7 +19,8 @@ nombreUsuario VARCHAR (25),
 apellidoUsuario VARCHAR (20),
 correoUsuario VARCHAR (50),
 fechaReserva DATE,
-comentario VARCHAR (100)
+comentario VARCHAR (100),
+asiento INT
 );
 
 
